6907f86a3014e82d80ee2927de68024b867d8854
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2019-03-13 20:38:50 -0500

    Get bot working

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 require('localenv');
-const Telegraf = require('telegra');
+const Telegraf = require('telegraf');
 const express = require('express');
+const crypto = require('crypto');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.start(({ reply }) => reply('Hello!'));
 bot.help(({ reply }) => reply('I don\'t work yet.'));
-bot.launch();
 
-bot.telegram.setWebhook('https://----.localtunnel.me/secret-path');
+const hash = crypto.createHash('sha256')
+    .update(process.env.BOT_TOKEN).digest('base64');
+console.log(hash);
+bot.telegram.setWebhook(`https://948693f0.ngrok.io/${hash}`);
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.use(bot.webhookCallback('/secret-path'));
+//app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bot.webhookCallback(`/${hash}`));
 app.listen(3000, () => {
-    console.log('App listening on port 3000');
+  console.log('Example app listening on port 3000!')
 });
