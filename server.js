a76300649dbc3ebfd0d762f3ccd8e386cd1518f9
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2019-03-17 13:55:34 -0500

    Use log module.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import localenv from 'localenv';
 import Telegraf from 'telegraf';
 import express from 'express';
 import crypto from 'crypto';
+import * as log from './utils/log';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -11,11 +12,10 @@ bot.help(({ reply }) => { reply("Here's what I can do: "); });
 
 const hash = crypto.createHash('sha256')
   .update(process.env.BOT_TOKEN).digest('base64');
-console.log(hash);
 bot.telegram.setWebhook(`https://355025bc.ngrok.io/${hash}`);
 
 const app = express();
 app.use(bot.webhookCallback(`/${hash}`));
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  log.logMessage('Example app listening on port 3000!');
 });
--- utils/log.js
+++ utils/log.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-console */
 /**
  *
  * @param {Error} err
  */
 const logError = err => console.error(err);
 
+/**
+ *
+ * @param {string} message
+ */
+const logMessage = message => console.log(message);
+
 export default logError;
+export { logMessage };
