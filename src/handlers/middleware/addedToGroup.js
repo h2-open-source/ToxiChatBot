8b4e26883c310d82f4966ccf5cf69514c3666f2e
Jimmy Hogobooom <jimmyhogoboom@gmail.com>
2020-09-06 00:23:25 -0500

    Add example of a button on a reply

--- src/handlers/middleware/addedToGroup.js
+++ src/handlers/middleware/addedToGroup.js
@@ -1,3 +1,5 @@
+import { Extra } from 'telegraf';
+
 // TODO: test this
 const addedToGroupHandler = async (ctx, next) => {
   const { message } = ctx;
@@ -9,6 +11,14 @@ const addedToGroupHandler = async (ctx, next) => {
 
   if (botWasAdded) {
     // TODO: save group
+
+    // TODO: Move this. This should be posted in response to an Admin in a DM with the bot
+    ctx.reply(
+      'Do you want to stay in this group? Click this button.',
+      Extra.HTML().markup(m => m.inlineKeyboard([
+        m.callbackButton('I want to stay!', 'optin'),
+      ])),
+    );
 
     ctx.reply(`Hi there, I'm ${process.env.BOT_NAME}`);
   }
