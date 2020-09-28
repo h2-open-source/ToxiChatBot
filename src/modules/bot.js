6e2e251e1877cd4322a0a5481360121004ddc3d0
Jimmy Hogoboom <jhogoboom@enlivenhq.com>
2020-09-27 19:52:13 -0500

    Set bot username to respond to @ commands

--- src/modules/bot.js
+++ src/modules/bot.js
@@ -1,5 +1,7 @@
 import Telegraf from 'telegraf';
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+    username: process.env.BOT_USERNAME,
+});
 
 export default bot;
