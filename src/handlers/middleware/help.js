721109a7e090fe9349262923c936ec958a5607a2
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2021-09-07 00:27:35 -0500

    return final message

--- src/handlers/middleware/help.js
+++ src/handlers/middleware/help.js
@@ -6,7 +6,7 @@ import { isPrivateChat } from '../../utils/telegramUtils';
 const handlePrivate = async ({ reply }) => {
   await reply("Here's what I can do: ");
   // TODO: Add better help text
-  await reply(
+  return reply(
     '/generate - Create the opt-in button\n' +
       "/list - Show a list of all the groups your buttons were forwarded to and see who's clicked them"
   );
