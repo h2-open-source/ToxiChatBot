af4ffccfa48440b34d570e545f6f84f8ee320734
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-02-15 17:17:53 -0600

    New globalConfig for jest-mongodb and a test for checking if a user is inserted.

---
+++ test/unit/mongodb-user.test.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should insert a user into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = { _id: 'some-user-id', id: '111111111' };
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({ _id: 'some-user-id' });
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
